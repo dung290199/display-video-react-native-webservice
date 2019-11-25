3041bb4b9dc89feb7bb6a9b304b4966eb6c9a7ed
HOANGSON <vuhson30799@gmail.com>
2019-11-25 16:26:34 +0700

    remove some info of the title

--- app/components/movieComponents.js
+++ app/components/movieComponents.js
@@ -1,65 +1,88 @@
 import React, {Component} from 'react';
-import {Button,Text,View, Image, Alert, Platform, TextInput, FlatList, SafeAreaView, Dimensions} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
 import WebView from 'react-native-webview';
-import { Searchbar } from 'react-native-paper';
+import {Searchbar} from 'react-native-paper';
 
 export default class MovieComponent extends Component {
-    state = {
-        firstQuery: '',
-      };
-    constructor(props) {
-        super(props);
-        this.state = {firstquery: ''};
-    }
+  state = {
+    firstQuery: '',
+  };
+  constructor(props) {
+    super(props);
+    this.state = {firstquery: ''};
+  }
 
-    getVideos() {
-        let object = this.props.movies;
-        let separator = "videos/";
-        let separatorLen = separator.length;
-        let l = object.length;
-        let videos = [];
-        for (let i = 0; i < l; i++){
-            let index = object[i].url.indexOf(separator);
-            if (index > 0){
-                let urlsplt = object[i].url.split(separator, 2);
-                let id = urlsplt[1].split("/");
-                let video = {url : "https://www.facebook.com/video/embed?video_id=" + id, title: object[i].title, description: object[i].description};
-                videos.push(video);
-            }
-        }
-        return videos;
+  getVideos() {
+    let object = this.props.movies;
+    let separator = 'videos/';
+    let separatorLen = separator.length;
+    let l = object.length;
+    let videos = [];
+    for (let i = 0; i < l; i++) {
+      let index = object[i].url.indexOf(separator);
+      if (index > 0) {
+        let urlsplt = object[i].url.split(separator, 2);
+        let id = urlsplt[1].split('/');
+        let video = {
+          url:
+            'https://www.facebook.com/video/embed?video_id=' +
+            id +
+            '&fullscreen=1',
+          title: this.getTitle(object[i].title),
+          description: object[i].description,
+        };
+        videos.push(video);
+      }
     }
+    return videos;
+  }
 
-    render() {
-        const {firstquery} = this.state;
-        {this.props.onFetchMovies();}
-        return (
-          <View style={{flex: 1}}>
-              <Searchbar
-                placeholder="Search"
-                onChangeText={query => { this.setState({ firstquery: query }); }}
-                value={firstquery}
-                icon={require('E:/REACT-NATIVE/test4/app/assets/icons/Search-05.png')}
-                clearIcon={require('E:/REACT-NATIVE/test4/app/assets/icons/Clear-01.png')}
-                />
-              <FlatList
-                  data={this.getVideos()}
-                  keyExtractor={(item) => item.toString()}
-                    renderItem={({item, index}) => 
-                        <View style={{flex : 1, flexDirection: 'row', height: 200, marginBottom: 10}}>
-                            <WebView
-                                style={{backgroundColor: 'grey', width: '100%'}}
-                                javaScriptEnabled={true}
-                                domStorageEnabled={true}
-                                source={{uri: item.url}}
-                            />
-                            <Text style={{width: 150, fontWeight: "bold"}}>${item.title}</Text>
-                        </View>
-                        }
-                />
-          </View>
-        );
+  getTitle(title) {
+    const REGEX = '#';
+    let titleArray = title.split(REGEX, 2);
+    return titleArray[0];
+  }
+  render() {
+    const {firstquery} = this.state;
+    {
+      this.props.onFetchMovies();
     }
-
+    return (
+      <View style={{flex: 1}}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={query => {
+            this.setState({firstquery: query});
+          }}
+          value={firstquery}
+          icon={require('D:/codegym/codegym/display-video-react-native-webservice/app/assets/icons/Search-05.png')}
+          clearIcon={require('D:/codegym/codegym/display-video-react-native-webservice/app/assets/icons/Clear-01.png')}
+        />
+        <FlatList
+          data={this.getVideos()}
+          keyExtractor={item => item.toString()}
+          renderItem={({item, index}) => (
+            <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                height: 200,
+                marginBottom: 10,
+              }}>
+              <WebView
+                contentInset={{top: 0, left: 0, bottom: 0, right: 0}}
+                style={{backgroundColor: 'grey', width: '100%'}}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+                source={{uri: item.url}}
+              />
+              <Text style={{width: 150, fontWeight: 'bold'}}>
+                ${item.title}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
+    );
+  }
 }
-
--- app/sagas/api.js
+++ app/sagas/api.js
@@ -1,13 +1,13 @@
-const urlGetMovies = 'http://164.132.226.137:9999/youtuber/feeds/searchByCommunityName?name=football4';
+const urlGetMovies =
+  'http://164.132.226.137:9999/youtuber/feeds/searchByCommunityName?name=football4';
 
 function* getMoviesFromApi() {
-    const response = yield fetch(urlGetMovies, {
-        method : 'GET',
-        body: '',
-    }).then(response => response.json());
+  const response = yield fetch(urlGetMovies, {
+    method: 'GET',
+    body: '',
+  }).then(response => response.json());
 
-
-    return yield (response);
+  return yield response;
 }
 
-export const Api = { getMoviesFromApi};
+export const Api = {getMoviesFromApi};
